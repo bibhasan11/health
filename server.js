f1083acd9b7f2dc6393e72e200e4637b063c96aa
Soura700 <108584756+Soura700@users.noreply.github.com>
2023-04-13 19:37:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,12 @@ const blogRoute = require("./routes/blog");
 const adminAuth = require("./middlewire/auth");
 const meetingRoute = require("./routes/meeting");
 const commentRoute = require("./routes/comment")
+const limitter = require("./middlewire/registerLimitter")
+const path = require("path")
 const blogAuth = require("./middlewire/blogAuth");
 const Post = require("./models/post");
 const authenticate = require("./middlewire/authenticate.js");
 const compression = require("compression");
-const serverless = require('serverless-http')
-const router = express.Router();
-
-
 // Step 1:
 dotenv.config();
 // Step 2:
@@ -27,6 +25,7 @@ app.use(express.json());
 app.use(compression())
 // Step 3:
 app.use(express.static("public"));
+
 // Step 4:
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -37,6 +36,7 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
+
 app.use(
   bodyParser.urlencoded({
     extended: false,
@@ -56,9 +56,15 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = twilio(accountSid,authToken);
 
-router.post('/send-sms',(req,res)=>{
+app.post('/send-sms',(req,res)=>{
+  // var id= req.params.id;
+  // var arr = id.split("@");
+  // var phone = arr[0];
+  // var link = arr[1];
   const {to,body} = req.body;
   client.messages.create({
+    // body:"The meeting link for scgeduled appointment is:"+link,
+    // to:"+91"+phone,
     body:body,
     to:to,
     from:'+14406643564'
@@ -72,16 +78,16 @@ router.post('/send-sms',(req,res)=>{
 })
 
 
-router.use("/api/appoitment",appoitmentRoute),
-router.use("/api/blog",blogRoute);
+app.use("/api/appoitment",appoitmentRoute),
+app.use("/api/blog",blogRoute);
 // app.use("/api/user/auth",limitter.registerLimitter,userRoute);
-router.use("/api/user/auth",userRoute);
-router.use("/api/admin/auth",adminRoute);
-router.use("/api/appoitment/meeting",meetingRoute),
-router.use("/api",commentRoute);
+app.use("/api/user/auth",userRoute);
+app.use("/api/admin/auth",adminRoute);
+app.use("/api/appoitment/meeting",meetingRoute),
+app.use("/api",commentRoute);
 
 
-router.get("/autocomplete",(req,res,next)=>{
+app.get("/autocomplete",(req,res,next)=>{
   var regex = new RegExp(req.query["term"],'i')
 
   var postFilter = Post.find({username:regex},{'username':1}).sort({"updated_at":-1}).sort({"created_at":-1}).limit(20);
@@ -109,85 +115,83 @@ router.get("/autocomplete",(req,res,next)=>{
 
 
 // Index
-router.get("/", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 // Blog sidebar
-router.get("/blog-sidebar", function (req, res) {
+app.get("/blog-sidebar", function (req, res) {
   res.sendFile(__dirname + "/public/blog-sidebar.html");
 });
 // Single-Blog
-router.get("/blog-single", function (req, res) {
+app.get("/blog-single", function (req, res) {
   res.sendFile(__dirname + "/public/blog-single.html");
 });
 // Department
-router.get("/department",function (req, res) {
+app.get("/department",function (req, res) {
   res.sendFile(__dirname + "/public/department.html");
 });
 // About
-router.get("/about", function (req, res) {
+app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/public/about.html");
 });
 // Contact
-router.get("/contact", function (req, res) {
+app.get("/contact", function (req, res) {
   res.sendFile(__dirname + "/public/contact.html");
 });
 //Create Blog Form
-router.get("/create/post",authenticate.isAuthenticated,(req,res)=>{
+app.get("/create/post",authenticate.isAuthenticated,(req,res)=>{
   res.sendFile(__dirname + "/public/create.html")
 });
 //Register Page
-router.get("/posts/account",(req, res) => {
+app.get("/posts/account",(req, res) => {
   res.sendFile(__dirname + "/public/register.html");
 });
 // Login Page
-router.get("/posts/login", (req, res) => {
+app.get("/posts/login", (req, res) => {
   res.sendFile(__dirname + "/public/login.html");
 });
 // User Login
-router.get("/log",adminAuth.isLogout,(req,res)=>{
+app.get("/log",adminAuth.isLogout,(req,res)=>{
   res.sendFile(__dirname + '/public/userLogin.html')
 })
 // User Register
-router.get("/reg",(req,res)=>{
+app.get("/reg",(req,res)=>{
   res.sendFile(__dirname + '/public/userRegister.html')
 })
 // Admin page
-router.get("/admin",adminAuth.isLogin,(req,res)=>{
+app.get("/admin",adminAuth.isLogin,(req,res)=>{
   res.sendFile(__dirname + '/public/admin.html')
 })
 // Patient Appoitment
-router.get("/appoinment", (req, res) => {
+app.get("/appoinment", (req, res) => {
   res.sendFile(__dirname + "/public/appoinment.html");
 });
 // UserPanel 
-router.get("/userpanel",(req,res)=>{
+app.get("/userpanel",(req,res)=>{
   res.sendFile(__dirname + '/public/userPanel.html')
 })
-router.get("/register/blog",(req,res)=>{
+app.get("/register/blog",(req,res)=>{
   res.sendFile(__dirname + '/public/register.html')
 })
-router.get("/login/blog",blogAuth.isLogout,(req,res)=>{
+app.get("/login/blog",blogAuth.isLogout,(req,res)=>{
   res.sendFile(__dirname + '/public/login.html')
 })
-router.get("/406/error",(req,res)=>{
+app.get("/406/error",(req,res)=>{
   res.sendFile(__dirname + '/public/406.html')
 })
-router.get("/500/error",(req,res)=>{
+app.get("/500/error",(req,res)=>{
   res.sendFile(__dirname + '/public/500.html')
 })
-router.get("/402/error",(req,res)=>{
+app.get("/402/error",(req,res)=>{
   res.sendFile(__dirname + '/public/402.html')
 })
-router.get("/forgot-password",(req,res)=>{
+app.get("/forgot-password",(req,res)=>{
   res.sendFile(__dirname + '/public/forgot.html')
 })
-router.get("/resetPassword",(req,res)=>{
+app.get("/resetPassword",(req,res)=>{
   res.sendFile(__dirname + '/public/reset-password.html');
 })
 // Setting the Port...
-// app.listen(5500, function () {
-//   console.log("Server listening to the port 5500");
-// });
-app.use('/.netlify/api', router);
-module.exports.handler = serverless(app);
+app.listen(5500, function () {
+  console.log("Server listening to the port 5500");
+});
